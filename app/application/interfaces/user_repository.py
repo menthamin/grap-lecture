991b9844ffce0c7d0cb34fe915bcc84a57c7c262
menthamin <menthamin@naver.com>
2022-07-31 14:21:30 +0900

    feat: add duplicate exception handling logic when creating user

--- app/application/interfaces/user_repository.py
+++ app/application/interfaces/user_repository.py
@@ -4,5 +4,10 @@ from app.domain.entity import Domain
 
 
 class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
     def create(self, model: Domain):
+        ...
+
+    @abc.abstractmethod
+    def find_one(self, model: Domain):
         ...
--- app/application/service/user.py
+++ app/application/service/user.py
@@ -9,5 +9,9 @@ class UserService:
     def create_user(self, user_name: str):
         # Save to DB
         _user = User(name=user_name)
+        # 데이터베이스에서 해당 이름이 있는지 확인하고, 있다면 Exception을 발생
+        # Check user name in database, when user name in database
+        if self.repository.find_one(model=_user):
+            raise ValueError("User name is duplicated")
         user = self.repository.create(_user)
         return user
